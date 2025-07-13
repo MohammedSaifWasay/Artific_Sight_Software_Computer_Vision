894c9acde277e6782918ee2b1aaefd8085052897
Mohammed Saif Wasay <91286715+MohammedSaifWasay@users.noreply.github.com>
2025-07-13 18:31:27 -0400

    Add files via upload

---
+++ mainal.py
@@ -0,0 +1,344 @@
+
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+import imutils
+import time
+import cv2
+import math
+from modules.detection import detect_people
+from scipy.spatial import distance as dist
+from modules.config import camera_no
+
+
+
+
+detection_model_path = 'haarcascade_files/haarcascade_frontalface_default.xml'
+emotion_model_path = 'models/_mini_XCEPTION.102-0.66.hdf5'
+ 
+# hyper-parameters for bounding boxes shape
+# loading models
+face_detection = cv2.CascadeClassifier(detection_model_path)
+emotion_classifier = load_model(emotion_model_path, compile=False)
+EMOTIONS = ["angry" ,"disgust","scared", "happy", "sad", "surprised",
+ "neutral"]
+
+# starting video streaming
+#cv2.namedWindow('my_face')
+
+labelsPath = "yolo-coco/coco.names"
+LABELS = open(labelsPath).read().strip().split("\n")
+np.random.seed(42)
+COLORS = np.random.randint(0, 
+                        255, 
+                        size=(len(LABELS), 3),
+                        dtype="uint8")
+
+weightsPath = "yolo-coco/yolov3.weights"
+configPath = "yolo-coco/yolov3.cfg"
+
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+# face mask classification
+confidence_threshold = 0.4
+
+
+if True:
+	# set CUDA as the preferable backend and target
+	print("")
+	print("[INFO] Looking for GPU")
+	net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+	net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+
+# load our serialized face detector model from disk
+print("loading face detector model...")
+prototxtPath = "models/deploy.prototxt"
+weightsPath = "models/res10_300x300_ssd_iter_140000.caffemodel"
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+
+
+# load the face mask detector model from disk
+model_store_dir= "models/classifier.model"
+maskNet = load_model(model_store_dir)
+
+cap = cv2.VideoCapture(0)       #Start Video Streaming
+while (cap.isOpened()):
+    ret, image = cap.read()
+
+    if ret == False:
+        break
+
+    image = cv2.resize(image, (1200, 800))
+    # cv2.namedWindow("output", cv2.WINDOW_NORMAL)
+    #image = cv2.resize(image,(400,400))
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_detection.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
+    
+    
+    canvas = np.zeros((600, 700, 3), dtype="uint8")
+    frameClone = image.copy()
+    if len(faces) > 0:
+        
+        faces = sorted(faces, reverse=True,
+        key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+        (fX, fY, fW, fH) = faces
+        roi = gray[fY:fY + fH, fX:fX + fW]
+        roi = cv2.resize(roi, (64, 64))
+        roi = roi.astype("float") / 255.0
+        roi = img_to_array(roi)
+        roi = np.expand_dims(roi, axis=0)
+        x = emotion_classifier.predict(roi)[0]
+        emotion_probability = np.max(x)
+        label = EMOTIONS[x.argmax()]
+
+        for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, x)):
+            
+                    # construct the label text
+                    text = "{}: {:.2f}%".format(emotion, prob * 100)
+
+                    # draw the label + probability bar on the canvas
+                   # emoji_face = feelings_faces[np.argmax(preds)]
+
+                    
+                    w = int(prob * 300)
+                    #print(w)
+                    cv2.rectangle(canvas, (7, (i * 35) + 5),
+                    (w, (i * 35) + 35), (0, 0, 255), -1)
+                    cv2.putText(canvas, text, (10, (i * 35) + 23),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45,
+                    (255, 255, 255), 2)
+                    cv2.putText(frameClone, label, (fX, fY - 40),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+                    cv2.rectangle(frameClone, (fX, fY), (fX + fW, fY + fH),
+                                  (0, 0, 255), 2)
+                    
+                
+                
+#    for c in range(0, 3):
+#        frame[200:320, 10:130, c] = emoji_face[:, :, c] * \
+#        (emoji_face[:, :, 3] / 255.0) + frame[200:320,
+#        10:130, c] * (1.0 - emoji_face[:, :, 3] / 255.0)
+
+
+    
+    cv2.imshow('my_face', frameClone)
+
+    img = imutils.resize(image, width=600)
+    height, width, channels = img.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+    results = detect_people(image, net, output_layers,
+                            personIdx=LABELS.index("person"))
+
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            classIDs = np.argmax(scores)
+            confidence = scores[classIDs]
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(classIDs)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    font = cv2.FONT_HERSHEY_PLAIN
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            if(classes[class_ids[i]] == classes[class_ids[i]] ):
+               
+                color = colors[i]
+                cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+                cv2.putText(image, label, (x, y + 30), font, 3, color, 3)
+                print(label)
+
+    # show the output frame
+    cv2.imshow("object Frame", image)
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.3)
+    ind = []
+    for i in range(0, len(class_ids)):
+        if (class_ids[i] == 0):
+            ind.append(i)
+    a = []
+    b = []
+    #color = (0, 255, 0)
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+            a.append(x)
+            b.append(y)
+    distance = []
+    nsd = []
+    for i in range(0, len(a) - 1):
+        for k in range(1, len(a)):
+            if (k == i):
+                break
+            else:
+                x_dist = (a[k] - a[i])
+                y_dist = (b[k] - b[i])
+                d = math.sqrt(x_dist * x_dist + y_dist * y_dist)
+                distance.append(d)
+                if (d <= 100.0):
+                    nsd.append(i)
+                    nsd.append(k)
+                nsd = list(dict.fromkeys(nsd))
+
+    color = (0, 0, 255)
+    for i in nsd:
+        (x, y) = (boxes[i][0], boxes[i][1])
+        (w, h) = (boxes[i][2], boxes[i][3])
+        cv2.rectangle(image, (x, y), (x + w, y + h), color, 1)
+        text = "Alert"
+        cv2.putText(image, text, (x, y - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+
+    color = (138, 68, 38)
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            if (i in nsd):
+                break
+            else:
+                (x, y) = (boxes[i][0], boxes[i][1])
+                (w, h) = (boxes[i][2], boxes[i][3])
+                cv2.rectangle(image, (x, y), (x + w, y + h), color, 1)
+                text = "SAFE"
+                cv2.putText(image, text, (x, y - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+
+
+
+    (h, w) = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(image, 1.0, (416, 416), (104.0, 177.0, 123.0))
+
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+
+    for i in range(0, detections.shape[2]):
+        confidence =detections[0, 0, i, 2]
+
+        if confidence > confidence_threshold:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            face = image[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+            face = np.expand_dims(face, axis=0)
+
+            # _____________________________________________________________________ #
+            #  Counting Sources
+
+            serious = set()
+            abnormal = set()
+
+            # ensure there are *at least* two people detections (required in
+            # order to compute our pairwise distance maps)
+            if len(results) >= 2:
+                # extract all centroids from the results and compute the
+                # Euclidean distances between all pairs of the centroids
+                centroids = np.array([r[2] for r in results])
+                D = dist.cdist(centroids, centroids, metric="euclidean")
+
+                # loop over the upper triangular of the distance matrix
+                for i in range(0, D.shape[0]):
+                    for j in range(i + 1, D.shape[1]):
+                        # check to see if the distance between any two
+                        # centroid pairs is less than the configured number of pixels
+                        if D[i, j] < 50:
+                            # update our violation set with the indexes of the centroid pairs
+                            serious.add(i)
+                            serious.add(j)
+                        # update our abnormal set if the centroid distance is below max distance limit
+                        if (D[i, j] < 80) and not serious:
+                            abnormal.add(i)
+                            abnormal.add(j)
+
+            # loop over the results
+            for (i, (prob, bbox, centroid)) in enumerate(results):
+                # extract the bounding box and centroid coordinates, then
+                # initialize the color of the annotation
+                (startX, startY, endX, endY) = bbox
+                (cX, cY) = centroid
+                color = (0, 255, 0)
+
+                # if the index pair exists within the violation/abnormal sets, then update the color
+                if i in serious:
+                    color = (0, 0, 255)
+                elif i in abnormal:
+                    color = (0, 255, 255) #orange = (0, 165, 255)
+
+                # draw (1) a bounding box around the person and (2) the
+                # centroid coordinates of the person,
+                # cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+                cv2.circle(image, (cX, cY), 2, color, 2)
+
+
+            # _____________________________________________________________________#
+
+            (mask, without_mask) = maskNet.predict(face)[0]
+            label = "Mask" if mask > without_mask else "No Mask"
+            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+            label = "{}: {:.2f}%".format(label, max(mask, without_mask) * 100)
+            c = cv2.putText(image, label, (startX, startY - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 1)
+            x = cv2.rectangle(image, (startX, startY), (endX, endY), color, 1)
+            text = "Total serious violations: {}".format(len(serious))
+            # cv2.putText(image, text, 
+            #             (10, image.shape[0] - 55),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.70, 
+            #             (0, 0, 255), 2)
+
+            text1 = "Total abnormal violations: {}".format(len(abnormal))
+            # cv2.putText(image, text1, 
+            #             (10, image.shape[0] - 25),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.70, 
+            #             (0, 255, 255), 2)
+
+
+            print("End of classifier")
+
+    # imS = cv2.resize(image, (960, 540))
+    # ver = np.vconcat([image, ig])
+    cv2.imshow("output frame",image)
+    # cv2.imshow("Face", x)
+    # cv2.imshow("Face", label)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
